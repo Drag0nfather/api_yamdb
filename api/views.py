8381f449218aeb0b649da7db4aadf285e9201148
Andrew <Andrey18651@gmail.com>
2021-03-23 18:06:56 +0300

    add UserViewSet pattern in api/views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from api.models import User
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
