7e71a338b8ccea42a1d89da7734d1a38f283599d
Andrew <Andrey18651@gmail.com>
2021-03-24 17:50:32 +0300

    update IsAdminPermission in api/permissions

--- api/permissions.py
+++ api/permissions.py
@@ -2,7 +2,12 @@ from rest_framework import permissions
 
 
 class IsAdminPermission(permissions.BasePermission):
-    pass
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_admin
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAdminOrReadOnlyPermission(permissions.BasePermission):
