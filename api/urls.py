3f0058a1a73b87f3999b1d6d664df39aec411afd
Andrew <Andrey18651@gmail.com>
2021-03-24 13:43:26 +0300

    import functions from api/mail to api/urls

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from api.mail import get_token_func, send_mail_func
 from api.views import UserViewSet
 
 router = DefaultRouter()
 
-router.register('users', UserViewSet)
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
