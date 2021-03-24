aa3f28546b1cb6c6a7a96c53edf597fb717c6506
Andrew <Andrey18651@gmail.com>
2021-03-24 14:03:15 +0300

    add confirmation code field to User model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from api.mail import generate_confirm_code
+
 
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     bio = models.CharField(max_length=500)
     email = models.EmailField(unique=True)
     role = models.TextChoices(value='user', names='user, admin, moderator')
+    confirmation_code = models.CharField(max_length=6, null=True, default=generate_confirm_code)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username',)
