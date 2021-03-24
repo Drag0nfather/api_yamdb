cb044b7f69baa90a5c1c55cdc31d105765cd529a
Andrew <Andrey18651@gmail.com>
2021-03-24 16:05:59 +0300

    add Permission patterns in api/permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,2 +1,18 @@
+from rest_framework import permissions
 
+
+class IsAdminPermission(permissions.BasePermission):
+    pass
+
+
+class IsAdminOrReadOnlyPermission(permissions.BasePermission):
+    pass
+
+
+class IsAuthorPermission(permissions.BasePermission):
+    pass
+
+
+class IsModerator(permissions.BasePermission):
+    pass
 
