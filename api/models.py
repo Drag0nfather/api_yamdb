2ca89c0ad69923290117185371284af740f3f69e
Andrew <Andrey18651@gmail.com>
2021-03-24 22:51:08 +0300

    add null=True to bio field in User class in api/models

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ class Roles(models.TextChoices):
 
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
-    bio = models.CharField(max_length=500)
+    bio = models.CharField(max_length=4000, null=True)
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=50, choices=Roles.choices)
     confirmation_code = models.CharField(max_length=6, null=True, default=generate_confirm_code)
