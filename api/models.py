f26e1a7c5403fba5bda9d35ae422728bd3d99fd1
Andrew <Andrey18651@gmail.com>
2021-03-24 13:56:48 +0300

    add User model with extra fields

--- api/models.py
+++ api/models.py
@@ -1,5 +1,12 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-User = get_user_model()
 
+class User(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    bio = models.CharField(max_length=500)
+    email = models.EmailField(unique=True)
+    role = models.TextChoices(value='user', names='user, admin, moderator')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
