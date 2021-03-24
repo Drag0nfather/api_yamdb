8dbdf4b0c3bbf7982fe092502da317139f0dbec8
Andrew <Andrey18651@gmail.com>
2021-03-24 18:04:55 +0300

    add 2 apiview patterns: RegisterView and TokenView

--- api/views.py
+++ api/views.py
@@ -1,10 +1,37 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
+from django_filters import filters
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from api.models import User
 from api.serializers import UserSerializer
 
 
+class RegisterView(APIView):
+    pass
+
+
+class TokenView(APIView):
+    pass
+
+
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    lookup_field = 'username'
+
+    @action(detail=False, permission_classes=(IsAuthenticated,),
+            methods=['get', 'patch'], url_path='me')
+    def get_or_update_self(self, request):
+        if request.method != 'GET':
+            serializer = self.get_serializer(
+                instance=request.user, data=request.data, partial=True)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            serializer = self.get_serializer(request.user, many=False)
+            return Response(serializer.data)
