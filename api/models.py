2bf01a1ddb2e48c0afef882e881e44bd35c011f1
Andrew <Andrey18651@gmail.com>
2021-03-24 14:49:20 +0300

    change
    "role" field in User class, add Role class

--- api/models.py
+++ api/models.py
@@ -4,11 +4,17 @@ from django.db import models
 from api.mail import generate_confirm_code
 
 
+class Roles(models.TextChoices):
+    USER = 'user'
+    MODERATOR = 'moderator'
+    ADMIN = 'admin'
+
+
 class User(AbstractUser):
     username = models.CharField(max_length=20, unique=True)
     bio = models.CharField(max_length=500)
     email = models.EmailField(unique=True)
-    role = models.TextChoices(value='user', names='user, admin, moderator')
+    role = models.CharField(max_length=50, choices=Roles.choices)
     confirmation_code = models.CharField(max_length=6, null=True, default=generate_confirm_code)
 
     USERNAME_FIELD = 'email'
