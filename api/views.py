7a870b800fc7f1fe3f26e64e67cb7621c09d9cbc
Andrew <Andrey18651@gmail.com>
2021-03-25 16:12:33 +0300

    update api/views: add get_token and send_verification_code functions

--- api/views.py
+++ api/views.py
@@ -1,27 +1,62 @@
+from django.contrib.auth.hashers import make_password, check_password
+from django.db.models import Max
+from rest_framework import status
 from rest_framework.views import APIView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django_filters import filters
 from rest_framework.response import Response
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
+from api.mail import generate_confirm_code, send_mail_func
 from api.models import User
-from api.serializers import UserSerializer
+from api.permissions import IsAdminPermission
+from api.serializers import UserSerializer, ConfirmationCodeSerializer, CheckConfirmationCodeSerializer
+
+BASE_USERNAME = 'User'
 
 
-class RegisterView(APIView):
-    pass
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def send_confirmation_code(request):
+    serializer = ConfirmationCodeSerializer(data=request.data)
+    email = request.data.get('email', False)
+    if serializer.is_valid():
+        confirmation_code = generate_confirm_code()
+        user = User.objects.filter(email=email).exists()
+        if not user:
+            User.objects.create_user(email=email)
+        User.objects.filter(email=email).update(
+            confirmation_code=make_password(confirmation_code, salt=None, hasher='default')
+        )
+        send_mail_func(email=email, confirmation_code=confirmation_code)
+        return Response(f'Код отправлен на адрес {email}', status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TokenView(APIView):
-    pass
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def get_token(request):
+    serializer = CheckConfirmationCodeSerializer(data=request.data)
+    if serializer.is_valid():
+        email = serializer.data.get('email')
+        confirmation_code = serializer.data.get('confirmation_code')
+        user = get_object_or_404(User, email=email)
+        if check_password(confirmation_code, user.confirmation_code):
+            token = AccessToken.for_user(user)
+            return Response({'token': f'{token}'}, status=status.HTTP_200_OK)
+        return Response({'confirmation_code': 'Неверный код подтверждения'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = 'username'
+    permission_classes = (IsAuthenticated, IsAdminPermission,)
 
     @action(detail=False, permission_classes=(IsAuthenticated,),
             methods=['get', 'patch'], url_path='me')
