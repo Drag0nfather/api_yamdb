a9fcb993cfa89f3c61090458b0b7dfade915b080
Sergey <thextreme.30@gmail.com>
2021-03-29 20:08:21 +0600

    fixed error

--- api/urls.py
+++ api/urls.py
@@ -16,12 +16,12 @@ from .views import (
 v1_router = DefaultRouter()
 
 v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/',
+    r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews_api_v1',
 )
 v1_router.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments_api_v1',
 )
